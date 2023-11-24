f017ecef3ac438aecc9ca369d664a2de2df1c686
Manas Majumder <manasmajumder247@gmail.com>
2023-11-24 20:03:08 +0600

    moving server

---
+++ include/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+<div class="signin-form">
+    <form action="" method="">
+        <div class="form-header">
+            <h3>Login</h3>
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="" class="form-control" placeholder="Enter your email" autocomplete="off" required>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="" class="form-control" placeholder="Password..." autocomplete="off" required>
+        </div>
+        <div class="small">
+            <a href="#">Forget password</a>
+        </div>
+
+        <div class="form-group">
+            <input type="submit" value="Login" class="btn btn-primary btn-block btn-lg">
+        </div>
+    </form>
+
+    <div class = "text-center small" style="color:#674288;">Don't have an account<a href="#" >Create Account</a> </div>
+</div>
+</body>
+</html>
+
+<?php
+
+echo "hello world";
